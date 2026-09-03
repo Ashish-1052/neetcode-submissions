25772a1c13918e600d3a446fce4f3517d8a705e4
Ashish Kedari <akedari1052@gmail.com>
2026-09-03 11:48:22 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        const result = [];
+        const tripletSet = new Set();
+        nums.sort((a, b) => a - b);
+        for (let i = 0; i < nums.length; i++) {
+            const sumTo = -nums[i];
+            let l = i + 1;
+            let r = nums.length - 1;
+            while (l < r) {
+                const mid = Math.floor((l + r) / 2);
+                const sum = nums[l] + nums[r];
+                if (sum > sumTo) {
+                    r--;
+                } else if (sum < sumTo) {
+                    l++;
+                } else {
+                    const newTriplet = [nums[i], nums[l], nums[r]]
+                    const key = newTriplet.join('#');
+                    if (!tripletSet.has(key)) {
+                        result.push(newTriplet);
+                        tripletSet.add(key);
+                    }
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return result;
+    }
+}
